5a740437baa80332e051f648185c10a1a6f6a8d5
sushant4032 <sushant4032@gmail.com>
2021-01-10 17:44:45 +0530

    First stateful widget

--- flutter/max1/lib/main.dart
+++ flutter/max1/lib/main.dart
@@ -4,10 +4,33 @@ void main() {
   runApp(App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return AppState();
+  }
+}
+
+class AppState extends State<App> {
+  var ques = ['one', 'two', 'three', 'four'];
+  var qi = 0;
+  void func1() {
+    setState(() {
+        qi++;
+    });
+    print('Answer chosen');
+  
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
-      home:Text('hello')
-    ,);
+      home: Scaffold(
+        appBar: AppBar(title: Text('My app')),
+        body: Column(children: <Widget>[
+          Text(ques[qi]),
+          RaisedButton(child: Text('click here'), onPressed: func1)
+        ]),
+      ),
+    );
   }
 }
